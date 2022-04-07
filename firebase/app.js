99746cb2c96e70a251b644167e6813b7c2d87c0a
omaryasser.cs@gmail.com <omaryasser.cs@gmail.com>
2022-04-07 16:59:04 +0200

    firebase added

---
+++ firebase/app.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
+// Add Firebase products that you want to use
+const firebaseConfig = {
+  apiKey: "AIzaSyAGaZVpMX1YZVNY8U5GYvuLbn4610d3fSc",
+  authDomain: "standby-e4a3a.firebaseapp.com",
+  projectId: "standby-e4a3a",
+  storageBucket: "standby-e4a3a.appspot.com",
+  messagingSenderId: "968894643985",
+  appId: "1:968894643985:web:55d67a5ff368d7b66856b0",
+  measurementId: "G-8GJL40QP83",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
