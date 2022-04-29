5a5ad3bdfaf64b810d0ef996eee4c04d02f2c463
omaryasser.cs@gmail.com <omaryasser.cs@gmail.com>
2022-04-29 11:54:40 +0200

    Slider now works

--- src/js/controller/profile.js
+++ src/js/controller/profile.js
@@ -1,2 +1,24 @@
 import "../../views/profile/profile.css";
 import "../../views/templates.js";
+
+const sections = document.getElementsByTagName("section");
+let secNumber = sections.length;
+
+document.getElementById("overview").addEventListener("click", () => {
+  var i = 0;
+  while (i < secNumber) {
+    sections[i].style.display = "none";
+    i++;
+  }
+  sections[0].style.display = "flex";
+});
+document.getElementById("favorites").addEventListener("click", () => {
+  var i = 0;
+  while (i < secNumber) {
+    sections[i].style.display = "none";
+    i++;
+  }
+  sections[1].style.display = "flex";
+});
+
+const test = () => {};
