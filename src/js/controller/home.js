66bad5f8932b873b2418a6eb23cb0c636006139e
omaryasser.cs@gmail.com <omaryasser.cs@gmail.com>
2022-05-10 03:02:24 +0200

    Stoped auth in home

--- src/js/controller/home.js
+++ src/js/controller/home.js
@@ -1,6 +1,6 @@
 import "/src/views/home/home.css";
 import "/src/views/templates.js";
-import "../firebase/Auth.js";
+// import "../firebase/Auth.js";
 import AOS from "/node_modules/aos/dist/aos.js";
 import "/node_modules/aos/dist/aos.css";
 AOS.init();
